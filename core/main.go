75ab1c02a7518f31ac4a2b4a968cecfc20d18493
Ch Meher Akshay <chakka.akshay@wipro.com>
2017-08-29 20:40:51 +0530

    json files removed

--- core/main.go
+++ core/main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	. "../models"
 	_ "../peer"
-	. "../controller"
-	. "../crypto"
-	"encoding/json"
+	// . "../controller"
+	// . "../crypto"
+	// "encoding/json"
+	"math/rand"
 	)
 
 
@@ -28,15 +29,19 @@ func main() {
 
 	//add more blocks 
 	bc := LoadBlockchain()
-	//create Account if necessary
-	CreateAccount("ravi")
-	privKey,pubKey := GetAccount("ravi")
-	data := "this is ravi main data to be signed with his key"
-	r,s := GetSignature(data,privKey)
-	var d = Data{data,pubKey,r,s}
-	bytes,_ := json.Marshal(d)
-	nextBlock := bc.GenerateNextBlock(string(bytes))
-	bc.AddBlock(nextBlock);
+	// // create Account if necessary
+	// CreateAccount("deepak")
+	
+	// for i:=0;i<100;i++ {
+	// 	privKey,pubKey := GetAccount("saurabh")
+	// 	value := randSeq(150)
+	// 	r,s := GetSignature(value,privKey)
+	// 	var d = Data{value,pubKey,r,s}
+	// 	bytes,_ := json.Marshal(d)
+	// 	nextBlock := bc.GenerateNextBlock(string(bytes))
+	// 	bc.AddBlock(nextBlock);
+	// }
+
 	//add more blocks 
 
 	bc.PrintChain();
@@ -47,4 +52,14 @@ func main() {
 	}
 	// StartPeer(&bc)
 
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
 }
